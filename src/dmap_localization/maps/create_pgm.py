821a54bf272fe87af41846c5c33c43f61874716c
vmazashvili <vanomazashvili@gmail.com>
2024-07-19 12:35:54 +0200

    Alligned map coords in RViz and Stage

--- src/dmap_localization/maps/create_pgm.py
+++ src/dmap_localization/maps/create_pgm.py
@@ -1,6 +1,6 @@
-# create_pgm.py
 width = 600
 height = 600
+border_thickness = 10  # Adjust this value to change the thickness of the border
 
 with open("simple_map.pgm", "w") as f:
     f.write("P2\n")  # PGM format
@@ -9,7 +9,11 @@ with open("simple_map.pgm", "w") as f:
 
     for y in range(height):
         for x in range(width):
-            if 200 < x < 400 and 200 < y < 400:
+            # Check if the current pixel is within the border area
+            if (x < border_thickness or x >= width - border_thickness or
+                y < border_thickness or y >= height - border_thickness):
+                f.write("0 ")  # Black border
+            elif 200 < x < 400 and 200 < y < 400:
                 f.write("0 ")  # Black obstacle
             else:
                 f.write("255 ")  # White free space
