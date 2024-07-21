08146e5ab45d6be3be6e0eae8e0f78a52f6ff798
vmazashvili <vanomazashvili@gmail.com>
2024-07-22 00:12:53 +0200

    Milestone 5: ICP Integration

--- src/dmap_localization/src/dmap_localizer.cpp
+++ src/dmap_localization/src/dmap_localizer.cpp
@@ -3,20 +3,25 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
 
 DMapLocalizer::DMapLocalizer()
-: initial_pose_received_(false)
+: initial_pose_received_(false),
+  map_received_(false),
+  map_cloud_(new pcl::PointCloud<pcl::PointXYZ>),
+  scan_cloud_(new pcl::PointCloud<pcl::PointXYZ>)
 {
     map_sub_ = nh_.subscribe("/map", 1, &DMapLocalizer::mapCallback, this);
     initial_pose_sub_ = nh_.subscribe("/initialpose", 1, &DMapLocalizer::initialPoseCallback, this);
     scan_sub_ = nh_.subscribe("/scan", 1, &DMapLocalizer::scanCallback, this);
-    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("map_point_cloud", 1);
+    point_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("aligned_scan_point_cloud", 1);
 }
 
 void DMapLocalizer::mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
     ROS_INFO("Map received");
     convertMapToPointCloud(msg);
+    map_received_ = true;
 }
 
 void DMapLocalizer::initialPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
@@ -36,19 +41,23 @@ void DMapLocalizer::initialPoseCallback(const geometry_msgs::PoseWithCovarianceS
 void DMapLocalizer::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     ROS_INFO("Laser scan received");
+    convertScanToPointCloud(msg);
+    if (map_received_)
+    {
+        alignScans();
+    }
 }
 
 void DMapLocalizer::convertMapToPointCloud(const nav_msgs::OccupancyGrid::ConstPtr& map)
 {
     pcl::PointCloud<pcl::PointXYZ> cloud;
 
-    // Iterate through the occupancy grid and convert occupied cells to points
     for (unsigned int y = 0; y < map->info.height; y++)
     {
         for (unsigned int x = 0; x < map->info.width; x++)
         {
             unsigned int i = x + y * map->info.width;
-            if (map->data[i] > 50)  // Threshold to consider cell as occupied
+            if (map->data[i] > 50)
             {
                 pcl::PointXYZ point;
                 point.x = x * map->info.resolution + map->info.origin.position.x;
@@ -59,12 +68,67 @@ void DMapLocalizer::convertMapToPointCloud(const nav_msgs::OccupancyGrid::ConstP
         }
     }
 
+    *map_cloud_ = cloud;
+
     sensor_msgs::PointCloud2 output;
     pcl::toROSMsg(cloud, output);
     output.header.frame_id = "map";
     point_cloud_pub_.publish(output);
 
-    ROS_INFO("Point cloud size: %lu", cloud.size());
+    ROS_INFO("Map point cloud size: %lu", cloud.size());
+}
+
+void DMapLocalizer::convertScanToPointCloud(const sensor_msgs::LaserScan::ConstPtr& scan)
+{
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    for (size_t i = 0; i < scan->ranges.size(); ++i)
+    {
+        if (std::isfinite(scan->ranges[i]))
+        {
+            float angle = scan->angle_min + i * scan->angle_increment;
+            pcl::PointXYZ point;
+            point.x = scan->ranges[i] * cos(angle);
+            point.y = scan->ranges[i] * sin(angle);
+            point.z = 0.0;
+            cloud.push_back(point);
+        }
+    }
+
+    *scan_cloud_ = cloud;
+
+    sensor_msgs::PointCloud2 output;
+    pcl::toROSMsg(cloud, output);
+    output.header.frame_id = scan->header.frame_id;
+    point_cloud_pub_.publish(output);
+
+    ROS_INFO("Scan point cloud size: %lu", cloud.size());
+}
+
+void DMapLocalizer::alignScans()
+{
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setInputSource(scan_cloud_);
+    icp.setInputTarget(map_cloud_);
+
+    pcl::PointCloud<pcl::PointXYZ> aligned_cloud;
+    icp.align(aligned_cloud);
+
+    if (icp.hasConverged())
+    {
+        Eigen::Matrix4f transformation = icp.getFinalTransformation();
+        ROS_INFO_STREAM("ICP converged with fitness score: " << icp.getFitnessScore());
+        ROS_INFO_STREAM("Transformation matrix:\n" << transformation);
+
+        sensor_msgs::PointCloud2 output;
+        pcl::toROSMsg(aligned_cloud, output);
+        output.header.frame_id = "map";
+        point_cloud_pub_.publish(output);
+    }
+    else
+    {
+        ROS_WARN("ICP did not converge.");
+    }
 }
 
 int main(int argc, char** argv)
