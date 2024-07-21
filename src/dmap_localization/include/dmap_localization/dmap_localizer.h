2536ed51d17fa71d2b5d95e60fb804d0199549d8
vmazashvili <vanomazashvili@gmail.com>
2024-07-21 23:22:42 +0200

    Subscribed to the  topic, conerted the occupancy grid map to a point cloud, verified the conversion by outputting the point cloud size

--- src/dmap_localization/include/dmap_localization/dmap_localizer.h
+++ src/dmap_localization/include/dmap_localization/dmap_localizer.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <sensor_msgs/LaserScan.h>
 
@@ -15,11 +16,13 @@ private:
     void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg);
     void initialPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+    void convertMapToPointCloud(const nav_msgs::OccupancyGrid::ConstPtr& map);
 
     ros::NodeHandle nh_;
     ros::Subscriber map_sub_;
     ros::Subscriber initial_pose_sub_;
     ros::Subscriber scan_sub_;
+    ros::Publisher point_cloud_pub_;
 };
 
 #endif // DMAP_LOCALIZER_H
