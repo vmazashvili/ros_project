04d1d1525c064083e852a6dd9207e692691f450a
vmazashvili <vanomazashvili@gmail.com>
2024-07-30 21:27:48 +0200

    Commented .cpp file

--- src/dmap_localization/src/dmap_localizer.cpp
+++ src/dmap_localization/src/dmap_localizer.cpp
@@ -5,6 +5,17 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
+/*
+- DMapLocalizer Constructor initializes the node handle `nh_`
+- Sets up subscribers for the laser scan, odometry, map topics
+- Initializes the publishers and point clouds. 
+  - The node listens to:
+    - `map`
+    - `scan`
+    - `odom`
+  - publishes:
+    - `localized_pose` (estimated pose)
+*/ 
 DMapLocalizer::DMapLocalizer()
   : nh_("~"), tf_listener_(tf_buffer_)
 {
@@ -18,6 +29,14 @@ DMapLocalizer::DMapLocalizer()
   scan_cloud_.reset(new pcl::PointCloud<pcl::PointXYZ>);
 }
 
+
+/*
+- Converts the received occupancy grid map into a point cloud representation `map_cloud_`
+- Iterates over the grid cells, checks if the cell is occupied
+- Adds the corresponding world coordinates to the `map_cloud_`
+
+- This point cloud is later used in ICP algorithm to alighn the laser scan data with the map
+*/
 void DMapLocalizer::mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
   ROS_INFO("Received map");
@@ -41,6 +60,16 @@ void DMapLocalizer::mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
   map_cloud_->is_dense = true;
 }
 
+
+/*
+- Processes the incoming laser scan data
+- Converts is into a point cloud
+- Checks if the range data is finite
+- Calculates the corresponding (x, y) coordinates in the robot's frame
+- Adds them to `scan_cloud`
+- Once the scan data is available and `map_cloud_` is not empty, 
+  it estimates robot's pose by calling `performLocalization()`
+*/
 void DMapLocalizer::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   ROS_INFO("Received a laser scan with %ld ranges", msg->ranges.size());
@@ -67,6 +96,10 @@ void DMapLocalizer::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
   }
 }
 
+/*
+- Updates the current odometry data `current_odom_` with the latest poition and orientation
+- This data can be used for dead reckoning or as an initial guess for localization
+*/
 void DMapLocalizer::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   ROS_INFO("Received odometry data with position (%f, %f) and orientation (%f)",
@@ -76,6 +109,16 @@ void DMapLocalizer::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
   current_odom_ = *msg;
 }
 
+/*
+- Alligns the `scan_cloud_` laser scan data with `map_cloud_` map data
+- Using the Iterative Closest Point (ICP) algorithm
+- If the alignment converges, the function:
+  - Extracts the transformation matrix
+  - Converts it to a `geometry_msgs::PoseStamped` message
+  - Publishes the pose
+    - Also calls `broadcastTransform` to:
+      - Broadcast the transform between the "map" and "base_link" frames
+*/
 void DMapLocalizer::performLocalization()
 {
   pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
@@ -113,6 +156,13 @@ void DMapLocalizer::performLocalization()
   }
 }
 
+
+/*
+- Broadcasts the transfrom from the "map" frame to the "base_link" frame,
+  based on the calculated localized pose
+- This broadcast allows other nodes to use the transform data for various tasks, 
+  such as visualizing the robot's position in RViz
+*/
 void DMapLocalizer::broadcastTransform(const geometry_msgs::PoseStamped& pose)
 {
   static tf2_ros::TransformBroadcaster br;
@@ -131,15 +181,14 @@ void DMapLocalizer::broadcastTransform(const geometry_msgs::PoseStamped& pose)
   br.sendTransform(transformStamped);
 }
 
+/*
+- Initializes the ROS node and the instance of `DMapLocalizer`
+- Calls `ros::spin()` to keep the node running, lietening for incoming
+  messages and calling the appropriate callbacks
+*/
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "dmap_localizer");
-  ros::NodeHandle nh;
-
-  // Subscribe to laser scan and odometry topics
-  //ros::Subscriber laser_sub = nh.subscribe("/scan", 10, scanCallback);
-  //ros::Subscriber odom_sub = nh.subscribe("/odom", 10, odomCallback)
-  
   DMapLocalizer localizer;
   ros::spin();
 
